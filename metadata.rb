96ad8ea887a6b022c85f1e5e91734047d4c0aadb
Tim Smith <tsmith84@gmail.com>
2015-09-09 22:15:09 -0700

    Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,11 @@
 name             'materialize'
-maintainer       'Adam Jacob'
-maintainer_email 'adam@getchef.com'
-license          'Apache v22'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license          'Apache 2.0'
 description      'Implements a materialized view pattern for searches'
-long_description 'Lets you materialize a search result and retrieve it easily'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+source_url 'https://github.com/chef-cookbooks/materialize' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/materialize/issues' if respond_to?(:issues_url)
 
