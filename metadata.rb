63a36082a816290fe4f67c295333d95de4a048c3
Tim Smith <tsmith@chef.io>
2016-09-16 09:53:45 -0700

    Require Chef 12.1+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Implements a materialized view pattern for searches'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-source_url 'https://github.com/chef-cookbooks/materialize' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/materialize/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/materialize'
+issues_url 'https://github.com/chef-cookbooks/materialize/issues'
 
-chef_version '>= 11' if respond_to?(:chef_version)
+chef_version '>= 12.1'
